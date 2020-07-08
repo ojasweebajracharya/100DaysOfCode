54eb879c5845f221cba31f90705fee8b0d395808
Ojaswee Professional <ojasweebajracharya@gmail.com>
2020-07-08 21:10:14 +0100

    Added to day 9's challenge

--- venv/9_Plates.py
+++ venv/9_Plates.py
@@ -6,27 +6,28 @@ K = platesInStack
 P = neededPlates
 '''
 
-class Stack:
-    def __init__(self):
-        self.stack = list()
-
-    def pushChar(self, ch):
-        self.stack.append(ch)
-
-    def popChar(self):
-        return self.stack.pop()
-
-
-def maxSumBeauty():
-    pass
+def maxSumBeauty(platesBeauty, stackCounter):
+    for i in range(n)
+    #format into {setnum}, {idx}, {sumbeforeit}]
+    #return [found,sumsofar]
 
 
 testCases = int(input())
+platesBeauty = []
 for i in range(testCases):
     numStacks, platesInStack, neededPlates = list(map(int, input().split(" ")))
     for j in range(numStacks):
-        platesBeauty = list(int, map(input().split()))
-        print("Case #{}: {}".format(i, maxSumBeauty(platesBeauty)))
+        platesBeauty.append(list(map(int, input().split())))
+
+    current = maxSumBeauty(platesBeauty, stackCounter)
+    while current[0] != neededPlates:
+        new = maxSumBeauty(platesBeauty, stackCounter)
+        current[0] += new[0]
+
+    print(current)
+    # print("Case #{}: {}".format(i, maxSumBeauty(platesBeauty)))
+
+
 
 
 
