4988624a1ec22f8c909dc8fa2c6ef1e23d632ef5
Ojaswee Professional <ojasweebajracharya@gmail.com>
2020-07-09 22:11:15 +0100

    Modified day 9

--- venv/9_Plates.py
+++ venv/9_Plates.py
@@ -5,26 +5,62 @@ N = numStacks
 K = platesInStack
 P = neededPlates
 '''
+import numpy as np
 
-def maxSumBeauty(platesBeauty, stackCounter):
-    for i in range(n)
-    #format into {setnum}, {idx}, {sumbeforeit}]
+def maxSumBeauty(platesBeauty):
+    organiseArr = []
+    for i in range(numStacks):
+        sortedArr = sorted(platesBeauty[i])
+        highestNum = sortedArr[platesInStack - 1]
+        # sumOfi = sum(platesBeauty[i[:platesBeauty[i].index(highestNum)]])
+        platesBeautyArr = np.array(platesBeauty)
+        sumOfi = platesBeautyArr[:,platesBeautyArr[i].index(highestNum)]
+
+        print(f"sumOfi: {sumOfi}")
+        organiseArr.append([platesBeauty[i].index(highestNum), highestNum, sumOfi, i])
+
+    return organiseArr
+    # format returned : {idx of highest num}, highestnum, {sumbeforeit}, idxInOriginal]
+
+
+
+# Main starts here:
     #return [found,sumsofar]
-
-
 testCases = int(input())
-platesBeauty = []
+
 for i in range(testCases):
+    total  = 0
+    found = 0
+
+    platesBeauty = []
     numStacks, platesInStack, neededPlates = list(map(int, input().split(" ")))
     for j in range(numStacks):
         platesBeauty.append(list(map(int, input().split())))
 
-    current = maxSumBeauty(platesBeauty, stackCounter)
-    while current[0] != neededPlates:
-        new = maxSumBeauty(platesBeauty, stackCounter)
-        current[0] += new[0]
+    currentMatrix = maxSumBeauty(platesBeauty)
 
-    print(current)
+    current2D = np.array(currentMatrix) # Gets rid of the commas, can use with numpy now
+    print(f"currentMatrix: {currentMatrix}")
+
+    # Sorts in order of highest sum value:
+    sortedMat = current2D[current2D[::-1,2].argsort()]
+    print(f"sortedMat: {sortedMat}")
+
+    k = 0
+    while found != neededPlates and k <= numStacks:
+        print(sortedMat[k][2])
+        # total += sortedMat[k][2]
+        #
+        # found += sortedMat[k][0] + 1
+        k += 1
+
+
+    # while current[0] != neededPlates:
+    #     new = maxSumBeauty(platesBeauty)
+    #     current[0] += new[0]
+
+
+
     # print("Case #{}: {}".format(i, maxSumBeauty(platesBeauty)))
 
 
